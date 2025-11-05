b0895e02f8c9ac35b7f6ab8d168e985c53e27ce3
Gabriela <gabriela.rgb@gmail.com>
2025-11-04 22:25:45 -0300

    mejoras en css

--- js/main.js
+++ js/main.js
@@ -1,76 +1,75 @@
 //Trivia de preguntas y respuestas por niveles
 
-// NOMBRE
-//================================
-
-//Muestro el modal cuando termina de cargarse el dom para obtener el nombre
-document.addEventListener("DOMContentLoaded", function () {
-    const nombreGuardado = localStorage.getItem("nombreUsuario")
-    // Checkeo si hay nombre
-    if (!nombreGuardado) {
-        // Si no hay nombre guardado, mostrar el modal usando la api de javascript de bootstrap
-        const modal = new bootstrap.Modal(document.getElementById("modalBienvenida"))
-        modal.show()
-    } else {
-        // Si ya hay nombre, mostrar saludo
-        const saludo = document.getElementById("row-welcome")
-        saludo.innerHTML = `¡Bienvenid@ de nuevo, ${nombreGuardado}!`
-    }
-
-    // Guardar el nombre cuando se hace clic en "Guardar"
-    const botonGuardar = document.getElementById("botonGuardar")
-    console.log(botonGuardar)
-    botonGuardar.onclick = () => {
-        //acá tendria que validar el campo
-        //.value.trim() elimina espacios en blanco adelante o detras del value
-        const nombreNuevo = document.getElementById("nombreUsuario").value.trim()
-        // Si nombreNuevo no está vacio
-        if (nombreNuevo !== "") {
-            // lo guardo en localStorage
-            localStorage.setItem("nombreUsuario", nombreNuevo)
-            const saludo = document.getElementById("row-welcome")
-            saludo.innerHTML = `¡Bienvenid@, ${nombreNuevo}!`
-        }
-    }
-})
-
-
+//======================
 // Inicializo variables
-// Inicializo los arrays de objetos
+//======================
 let preguntasDisponibles = []
 let preguntasJugadas = []
 
-// SCORE
-//============================================
-
-//Reviso si el usuario ya jugó, declaro la var y traigo los puntos guardados en localStorage
+// Cargo localStorages
+let nombreGuardado = localStorage.getItem("nombreUsuario")
 let score = localStorage.getItem("score")
-
-// si está undefined creo la clave
-if (score == undefined) {
-    localStorage.setItem("score", 0)
-    score = 0
-}
-
-// Muestro el score
-let rowTitle = document.getElementById("row-title")
-rowTitle.innerHTML = "Tenes " + score + " puntos"
 
 // Vinculo el div que contiene las preguntas
 let jeopardyBoard = document.getElementById("jeopardy-board")
-// let preguntasContainer = document.getElementById("preguntas-container")
-// // recordar q los arrays de obj se recorren con la f de orden sup/ metodo "forEach", no usar "for-of"
 
 // Vinculo el json con la data
 const URL = "./db/data.json"
 
 
+//======================================================================
+// Funcion que chequea si el usuario es nuevo o si lo saludo nuevamente
+//======================================================================
+function saludo() {
+    if (nombreGuardado !== null) {
+        // traigo los localstorage guardados
+        // NOMBRE y saludo
+        const saludo = document.getElementById("row-welcome")
+        saludo.innerHTML = `¡Bienvenid@ de nuevo, ${nombreGuardado}!`
 
+        //SCORE y puntaje
+        let rowTitle = document.getElementById("row-title")
+        rowTitle.innerHTML = "Tenes " + score + " puntos"
+
+        console.log(nombreGuardado + " - " + score)
+
+    } else {
+        //Si es usuario nuevo creo la clave e inicializo el score en 0
+        localStorage.setItem("score", 0)
+        let score = 0
+        let rowTitle = document.getElementById("row-title")
+        rowTitle.innerHTML = "Tenes " + score + " puntos"
+
+        //Si es usuario nuevo capturo el nombre y creo la clave
+        const modal = new bootstrap.Modal(document.getElementById("modalBienvenida"))
+        modal.show()
+        const botonGuardar = document.getElementById("botonGuardar")
+
+        botonGuardar.onclick = () => {
+            //acá tendria que validar el campo
+            //.value.trim() elimina espacios en blanco adelante o detras del value
+            const nombreNuevo = document.getElementById("nombreUsuario").value.trim()
+            // Si nombreNuevo no está vacio
+            if (nombreNuevo !== "") {
+                // lo guardo en localStorage
+                localStorage.setItem("nombreUsuario", nombreNuevo)
+                const bienvenide = document.getElementById("row-welcome")
+                bienvenide.innerHTML = `¡Bienvenid@, ${nombreNuevo}!`
+            }
+        }
+    }
+}
+
+
+//==========================================================
+// Simulo consulta a bbdd externa para obtener preguntas
+//==========================================================
 function obtenerPreguntas() {
     fetch(URL)
         .then(response => response.json())
         .then(data => {
             preguntasDisponibles = data
+            // ordeno preguntas s/ puntos de menor a mayor
             preguntasDisponibles.sort((a, b) => a.puntos - b.puntos)
             armarJeopardy(data)
         })
@@ -79,15 +78,22 @@ function obtenerPreguntas() {
 }
 
 
+//=================
+// Armo la trivia
+//=================
 function armarJeopardy(preguntas) {
     preguntas.forEach(pregunta => {
         const contenedor = document.createElement("div")
-        contenedor.innerHTML = `<button class="jugar cell valor-${pregunta.puntos}" id="${pregunta.id}">${pregunta.puntos}</button>`
+        contenedor.innerHTML = `<button class="jugar cell categ-${pregunta.categoria}" id="${pregunta.id}">${pregunta.puntos}</button>`
         jeopardyBoard.appendChild(contenedor)
     })
     escucharBotones()
 }
 
+
+//=======================================
+// Escucho los botones de las preguntas
+//=======================================
 function escucharBotones() {
     let botones = document.querySelectorAll(".jugar")
     //recordar q es una lista de nodos
@@ -109,6 +115,9 @@ function escucharBotones() {
     })
 }
 
+
+//Muestro el modal cuando termina de cargarse el dom para obtener el nombre
+document.addEventListener("DOMContentLoaded", saludo)
 
 // Llamo a la funcion principal
 obtenerPreguntas()
